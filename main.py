49f6cfdbd8a301cad44422cd2b4cc6c0af899a87
AbdelRahmanHelal1 <bdwhlal47@gmail.com>
2025-07-07 10:23:34 +0300

    init send_image_and_treatment file

--- main.py
+++ main.py
@@ -24,38 +24,6 @@ class_names = model.names
 cap = cv2.VideoCapture(r"istockphoto-2150887655-640_adpp_is.mp4")
 
 
-def send_image(bot_token, chat_id, image_path):
-    """
-    Sends an image to a Telegram chat.
-
-    Parameters:
-    - bot_token: The token for the Telegram bot.
-    - chat_id: The ID of the Telegram chat to send the image to.
-    - image_path: The file path of the image to send.
-    """
-    url = f'https://api.telegram.org/bot{bot_token}/sendPhoto'
-    files = {'photo': open(image_path, 'rb')}
-    data = {'chat_id': chat_id}
-    response = requests.post(url, files=files, data=data)
-    return response.json()
-
-
-def send_image_and_treatment(annotated_image_path, detected_diseases):
-    """
-    Send image and treatment information to Telegram.
-
-    Parameters:
-    - annotated_image_path: The path to the annotated image.
-    - detected_diseases: A list of detected diseases and their treatments.
-    """
-    send_image(TOKEN, chat_id, annotated_image_path)
-    for disease, treatment in detected_diseases.items():
-        message = f"Disease: {disease}\nTreatment: {treatment}"
-        url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-        data = {'chat_id': chat_id, 'text': message}
-        requests.post(url, data=data)
-
-
 # Timer setup
 start_time = time.time()
 send_interval = 10  # 10 minutes in seconds
